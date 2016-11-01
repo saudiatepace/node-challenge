114ccf5cc7f10d044a536377ca61e911b650e7ae
saudia tepace <saudiatepace@yahoo.com>
2016-11-01 17:21:43 +0800

    feature: add api method for getting to philosophy

---
+++ app.js
@@ -0,0 +1,29 @@
+//'use strict';
+
+var express       = require('express');
+var routes        = require('./routes');
+var getPhilosophy = require('./routes/getPhilosophy');
+var http          = require('http');
+var path          = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 8000);
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.post('/getPhilosophy', getPhilosophy.getPhilosophy);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ routes/getPhilosophy.js
@@ -0,0 +1,111 @@
+'use strict';
+
+var request = require( 'request' );
+var cheerio = require( 'cheerio' );
+var _       = require( 'underscore' );
+
+var visited  = [];
+var path     = [];
+
+function checkLink( link ) {
+ var url = link.attr('href');
+
+ var linkOk = visited.indexOf(url) === -1 &&
+	 url.indexOf('Help:') === -1 &&
+	 url.indexOf('File:') === -1 &&
+	 url.indexOf('Wikipedia:') === -1 &&
+	 url.indexOf('wiktionary.org/') === -1 &&
+	 url.indexOf('/wiki/') !== -1;
+
+ if( linkOk ) {
+
+	 // Check if the link is between parenthesis
+	 var contentHtml = link.closest('p').length > 0 ? link.closest('p').html() : '';
+	 if( contentHtml !== '' ) {
+		 var linkHtml = 'href="' + url + '"';
+		 var contentBeforeLink = contentHtml.split(linkHtml)[0];
+		 var openParenthesisCount = contentBeforeLink.split('(').length - 1;
+		 var closeParenthesisCount = contentBeforeLink.split(')').length - 1;
+		 linkOk = openParenthesisCount <= closeParenthesisCount;
+	 }
+ }
+
+ if(linkOk) {
+	 // Check that the link is not in italic
+	 linkOk = link.parents('i').length === 0;
+ }
+
+ return linkOk;
+}
+
+function doGetPhilosophy (req, res) {
+	var selector = '#mw-content-text > p a, #mw-content-text > ul a';
+	var title    = req;
+	var url      = 'http://en.wikipedia.com/wiki/'+ title;
+
+	request(url, function ( error, response, data ) {
+		if (error || response.statusCode !== 200) {
+			console.log('Article does not exist');
+			if ( error ) {
+				return 'Article does not exist';
+			}
+		}
+		var $ = cheerio.load(data);
+		var content = $('#mw-content-text');
+		var noArticle = $('.noarticvarext');
+
+		if (!!content.html() && !noArticle.html()) {
+
+			// Add the title of the current page in the 'path' array
+			var curr_title = $('#firstHeading').text();
+			path.push(curr_title);
+
+			// displaying the current title in console
+			console.log("=>" + curr_title);
+
+			// If we arrived at 'Philosophy', VICTORY!
+			if (curr_title === 'Philosophy') {
+				var pathArr = _.extend( {}, path );
+				return res.json( pathArr );
+			}
+
+			// Check if link is OK, if not then proceed to the next
+			// one until the link is good!
+			var link;
+			var ii = 0;
+			while (1){
+			 link = $( selector ).eq( ii );
+
+			 if( checkLink(link ) ) {
+					url = link.attr('href');
+					visited.push(url);
+					break;
+			 }
+			 ii++;
+			}
+
+			// splitting the next url link to serve as parameter for the
+			// recursive function
+			var nextUrl   = url.split( "/" );
+			var nextTitle = nextUrl[2];
+
+			// perform recursion
+			doGetPhilosophy( nextTitle, res );
+
+		} else { // If it is not an actual Wikipedia article, callback error and return
+				console.log('Article does not exist');
+				if ( error ) {
+					return 'Article does not exist';
+				}
+		}
+	} );
+}
+
+// method that's being called in POST method
+exports.getPhilosophy = function( req, res ) {
+	var response = res;
+	var request  = req;
+
+	doGetPhilosophy( request.query.title, response );
+};
+
